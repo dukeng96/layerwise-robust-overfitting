7783caf1a4bddcdfc913ff1006043133a3b269ed
duke96 <Innovation123>
2023-05-14 21:22:41 +1000

    Add train vgg with adversarial data

--- train_vgg_freeze.py
+++ train_vgg_freeze.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import shutil
 import time
+import csv
 
 import torch
 import torch.nn as nn
@@ -12,6 +13,7 @@ import torch.utils.data
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 import vgg
+from utils import *
 
 model_names = sorted(name for name in vgg.__dict__
     if name.islower() and not name.startswith("__")
@@ -54,15 +56,110 @@ parser.add_argument('--cpu', dest='cpu', action='store_true',
 parser.add_argument('--save-dir', dest='save_dir',
                     help='The directory used to save the trained models',
                     default='save_temp', type=str)
+parser.add_argument('--attack', default='pgd', type=str, choices=['pgd', 'fgsm', 'free', 'none'])
+parser.add_argument('--epsilon', default=8, type=int)
+parser.add_argument('--attack-iters', default=10, type=int)
+parser.add_argument('--attack-iters-test', default=20, type=int)
+parser.add_argument('--restarts', default=1, type=int)
+parser.add_argument('--pgd-alpha', default=2, type=float)
+parser.add_argument('--norm', default='l_inf', type=str, choices=['l_inf', 'l_2'])
+parser.add_argument('--target-layers', nargs='+', type=int)
+parser.add_argument('--freeze', default=False, action='store_true')
+
+mu = torch.tensor(cifar10_mean).view(3, 1, 1).to(device)
+std = torch.tensor(cifar10_std).view(3, 1, 1).to(device)
+upper_limit, lower_limit = 1,0
+
+def normalize(X):
+    return (X - mu) / std
+
+def clamp(X, lower_limit, upper_limit):
+    return torch.max(torch.min(X, upper_limit), lower_limit)
 
 
+def mixup_data(x, y, alpha=1.0):
+    '''Returns mixed inputs, pairs of targets, and lambda'''
+    if alpha > 0:
+        lam = np.random.beta(alpha, alpha)
+    else:
+        lam = 1
+
+    batch_size = x.size()[0]
+    index = torch.randperm(batch_size).to(device)
+
+    mixed_x = lam * x + (1 - lam) * x[index, :]
+    y_a, y_b = y, y[index]
+    return mixed_x, y_a, y_b, lam
+
+
+def mixup_criterion(criterion, pred, y_a, y_b, lam):
+    return lam * criterion(pred, y_a) + (1 - lam) * criterion(pred, y_b)
+
+
+def attack_pgd(model, X, y, epsilon, alpha, attack_iters, restarts,
+               norm, early_stop=False,
+               mixup=False, y_a=None, y_b=None, lam=None):
+    max_loss = torch.zeros(y.shape[0]).to(device)
+    max_delta = torch.zeros_like(X).to(device)
+    for _ in range(restarts):
+        delta = torch.zeros_like(X).to(device)
+        if norm == "l_inf":
+            delta.uniform_(-epsilon, epsilon)
+        elif norm == "l_2":
+            delta.normal_()
+            d_flat = delta.view(delta.size(0),-1)
+            n = d_flat.norm(p=2,dim=1).view(delta.size(0),1,1,1)
+            r = torch.zeros_like(n).uniform_(0, 1)
+            delta *= r/n*epsilon
+        else:
+            raise ValueError
+        delta = clamp(delta, lower_limit-X, upper_limit-X)
+        delta.requires_grad = True
+        for _ in range(attack_iters):
+            output = model(normalize(X + delta))
+            if early_stop:
+                index = torch.where(output.max(1)[1] == y)[0]
+            else:
+                index = slice(None,None,None)
+            if not isinstance(index, slice) and len(index) == 0:
+                break
+            if mixup:
+                criterion = nn.CrossEntropyLoss()
+                loss = mixup_criterion(criterion, model(normalize(X+delta)), y_a, y_b, lam)
+            else:
+                loss = F.cross_entropy(output, y)
+            loss.backward()
+            grad = delta.grad.detach()
+            d = delta[index, :, :, :]
+            g = grad[index, :, :, :]
+            x = X[index, :, :, :]
+            if norm == "l_inf":
+                d = torch.clamp(d + alpha * torch.sign(g), min=-epsilon, max=epsilon)
+            elif norm == "l_2":
+                g_norm = torch.norm(g.view(g.shape[0],-1),dim=1).view(-1,1,1,1)
+                scaled_g = g/(g_norm + 1e-10)
+                d = (d + scaled_g*alpha).view(d.size(0),-1).renorm(p=2,dim=0,maxnorm=epsilon).view_as(d)
+            d = clamp(d, lower_limit - x, upper_limit - x)
+            delta.data[index, :, :, :] = d
+            delta.grad.zero_()
+        if mixup:
+            criterion = nn.CrossEntropyLoss(reduction='none')
+            all_loss = mixup_criterion(criterion, model(normalize(X+delta)), y_a, y_b, lam)
+        else:
+            all_loss = F.cross_entropy(model(normalize(X+delta)), y, reduction='none')
+        max_delta[all_loss >= max_loss] = delta.detach()[all_loss >= max_loss]
+        max_loss = torch.max(max_loss, all_loss)
+    return max_delta
+
+
+args = parser.parse_args()
 best_prec1 = 0
+epsilon = (args.epsilon / 255.)
+pgd_alpha = (args.pgd_alpha / 255.)
 
 
 def main():
-    global args, best_prec1
-    args = parser.parse_args()
-
+    RESULT_PATH = f"{args.data_dir}/result/freeze_dataset=cifar10_lr=0.05_norm={args.norm}_target_layers={args.target_layers}_model=vgg19_atk={args.attack}.csv"
 
     # Check the save_dir exists or not
     if not os.path.exists(args.save_dir):
@@ -74,7 +171,7 @@ def main():
     if args.cpu:
         model.cpu()
     else:
-        model.cuda()
+        model.to(device)
 
     # optionally resume from a checkpoint
     if args.resume:
@@ -91,15 +188,15 @@ def main():
 
     cudnn.benchmark = True
 
-    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                     std=[0.229, 0.224, 0.225])
+    # normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],  # Change 1
+    #                                  std=[0.229, 0.224, 0.225])
 
     train_loader = torch.utils.data.DataLoader(
         datasets.CIFAR10(root=args.data_dir, train=True, transform=transforms.Compose([
             transforms.RandomHorizontalFlip(),
             transforms.RandomCrop(32, 4),
             transforms.ToTensor(),
-            normalize,
+            # normalize,
         ]), download=True),
         batch_size=args.batch_size, shuffle=True,
         num_workers=args.workers, pin_memory=True)
@@ -107,7 +204,7 @@ def main():
     val_loader = torch.utils.data.DataLoader(
         datasets.CIFAR10(root=args.data_dir, train=False, transform=transforms.Compose([
             transforms.ToTensor(),
-            normalize,
+            # normalize,
         ])),
         batch_size=args.batch_size, shuffle=False,
         num_workers=args.workers, pin_memory=True)
@@ -117,7 +214,7 @@ def main():
     if args.cpu:
         criterion = criterion.cpu()
     else:
-        criterion = criterion.cuda()
+        criterion = criterion.to(device)
 
     if args.half:
         model.half()
@@ -135,14 +232,22 @@ def main():
         adjust_learning_rate(optimizer, epoch)
 
         # train for one epoch
-        train(train_loader, model, criterion, optimizer, epoch)
+        train_robust_loss, train_robust_acc, train_loss, train_acc, train_n = train(train_loader, model, criterion, optimizer, epoch)
 
         # evaluate on validation set
-        prec1 = validate(val_loader, model, criterion)
+        test_robust_loss, test_robust_acc, test_loss, test_acc, test_n = validate(val_loader, model, criterion)
+
+        # Write result to file
+        data = [epoch, train_acc / train_n, train_loss / train_n, train_robust_acc / train_n,
+                train_robust_loss / train_n,
+                test_acc / test_n, test_loss / test_n, test_robust_acc / test_n, test_robust_loss / test_n]
+        with open(RESULT_PATH, 'a', encoding='UTF8') as f:
+            writer = csv.writer(f)
+            writer.writerow(data)
 
         # remember best prec@1 and save checkpoint
-        is_best = prec1 > best_prec1
-        best_prec1 = max(prec1, best_prec1)
+        # is_best = prec1 > best_prec1
+        # best_prec1 = max(prec1, best_prec1)
         # save_checkpoint({
         #     'epoch': epoch + 1,
         #     'state_dict': model.state_dict(),
@@ -158,37 +263,59 @@ def train(train_loader, model, criterion, optimizer, epoch):
     data_time = AverageMeter()
     losses = AverageMeter()
     top1 = AverageMeter()
+    train_loss = 0
+    train_acc = 0
+    train_robust_loss = 0
+    train_robust_acc = 0
+    train_n = 0
 
     # switch to train mode
     model.train()
 
     end = time.time()
+
     for i, (input, target) in enumerate(train_loader):
 
         # measure data loading time
         data_time.update(time.time() - end)
 
         if args.cpu == False:
-            input = input.cuda()
-            target = target.cuda()
+            input = input.to(device)
+            target = target.to(device)
         if args.half:
             input = input.half()
 
+        # generate adversarial example
+        if args.attack == 'pgd':
+            delta = attack_pgd(model, input, target, epsilon, pgd_alpha, args.attack_iters, args.restarts, args.norm)
+            delta = delta.detach()
+        elif args.attack == 'fgsm':
+            delta = attack_pgd(model, input, target, epsilon, args.fgsm_alpha * epsilon, 1, 1, args.norm)
+        # Standard training
+        elif args.attack == 'none':
+            delta = torch.zeros_like(input)
+
         # compute output
         output = model(input)
+        robust_output = model(normalize(torch.clamp(input + delta[:input.size(0)], min=lower_limit, max=upper_limit))) # Change 2
         loss = criterion(output, target)
+        robust_loss = criterion(robust_output, target)
 
         # compute gradient and do SGD step
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
-        output = output.float()
-        loss = loss.float()
         # measure accuracy and record loss
-        prec1 = accuracy(output.data, target)[0]
-        losses.update(loss.item(), input.size(0))
+        prec1 = accuracy(robust_output.float().data, target)[0]
+        losses.update(robust_loss.float().item(), input.size(0))
         top1.update(prec1.item(), input.size(0))
+
+        train_robust_loss += robust_loss.item() * target.size(0)
+        train_robust_acc += (robust_output.max(1)[1] == target).sum().item()
+        train_loss += loss.item() * target.size(0)
+        train_acc += (output.max(1)[1] == target).sum().item()
+        train_n += target.size(0)
 
         # measure elapsed time
         batch_time.update(time.time() - end)
@@ -203,6 +330,8 @@ def train(train_loader, model, criterion, optimizer, epoch):
                       epoch, i, len(train_loader), batch_time=batch_time,
                       data_time=data_time, loss=losses, top1=top1))
 
+        return train_robust_loss, train_robust_acc, train_loss, train_acc, train_n
+
 
 def validate(val_loader, model, criterion):
     """
@@ -211,6 +340,11 @@ def validate(val_loader, model, criterion):
     batch_time = AverageMeter()
     losses = AverageMeter()
     top1 = AverageMeter()
+    test_loss = 0
+    test_acc = 0
+    test_robust_loss = 0
+    test_robust_acc = 0
+    test_n = 0
 
     # switch to evaluate mode
     model.eval()
@@ -218,24 +352,46 @@ def validate(val_loader, model, criterion):
     end = time.time()
     for i, (input, target) in enumerate(val_loader):
         if args.cpu == False:
-            input = input.cuda()
-            target = target.cuda()
+            input = input.to(device)
+            target = target.to(device)
 
         if args.half:
             input = input.half()
 
+        # generate adversarial example
+        if args.attack == 'pgd':
+            delta = attack_pgd(model, input, target, epsilon, pgd_alpha, args.attack_iters_test, args.restarts, args.norm)
+            delta = delta.detach()
+        elif args.attack == 'fgsm':
+            delta = attack_pgd(model, input, target, epsilon, args.fgsm_alpha * epsilon, 1, 1, args.norm)
+        # Standard training
+        elif args.attack == 'none':
+            delta = torch.zeros_like(input)
+
         # compute output
         with torch.no_grad():
+            # compute output
             output = model(input)
+            robust_output = model(
+                normalize(torch.clamp(input + delta[:input.size(0)], min=lower_limit, max=upper_limit)))  # Change 2
             loss = criterion(output, target)
+            robust_loss = criterion(robust_output, target)
 
         output = output.float()
+        robust_output = robust_output.float()
         loss = loss.float()
+        robust_loss = robust_loss.float()
 
         # measure accuracy and record loss
-        prec1 = accuracy(output.data, target)[0]
-        losses.update(loss.item(), input.size(0))
+        prec1 = accuracy(robust_output.float().data, target)[0]
+        losses.update(robust_loss.float().item(), input.size(0))
         top1.update(prec1.item(), input.size(0))
+
+        test_robust_loss += robust_loss.item() * target.size(0)
+        test_robust_acc += (robust_output.max(1)[1] == target).sum().item()
+        test_loss += loss.item() * target.size(0)
+        test_acc += (output.max(1)[1] == target).sum().item()
+        test_n += target.size(0)
 
         # measure elapsed time
         batch_time.update(time.time() - end)
@@ -252,7 +408,7 @@ def validate(val_loader, model, criterion):
     print(' * Prec@1 {top1.avg:.3f}'
           .format(top1=top1))
 
-    return top1.avg
+    return test_robust_loss, test_robust_acc, test_loss, test_acc, test_n
 
 def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
     """
